7ddbe53998ec02d7b965cb12c619c380e12d781c
vifelisberto <vi.felis@hotmail.com>
2020-10-26 20:10:49 -0300

    instrucoes

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
 # APS Linguagens Formais e Autômatos
 
-Deve ser modelado e implementado um autômato que faça a validação de atribuições. Uma atribuição possui a seguinte sintaxe:
+## Dependências
 
+[Java JDK / JRE](https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html)
+
+## Executar
+
+### VSCode
+
+Caso tenha o visual studio code, apenas abra o arquivo `src/main/java/AssignmentAutomaton.java` do projeto e aperte F5, para executar.
+
+### Terminal
+
+Dentro da pasta do projeto rode os seguintes comandos:
+
+```shell
+javac src/main/java/AssignmentAutomaton.java
+java src/main/java/AssignmentAutomaton
 ```
-identificador op_atrib (identificador | numero) pv
-```
 
-Sendo que:
+O comando `javac` compila o projeto e o comando `java` executa o arquivo compilado.
 
-- identificador: inicia por uma letra minúscula, seguido por uma quantidade qualquer de letras minúsculas, dígitos ou underline.
-
-- op_atrib: é representado pelo caractere “=”
-
-- numero: inicia por um dígito, seguido por uma quantidade qualquer de dígitos, podendo ter opcionalmente a parte fracionária. Caso ele tenha a parte fracionária, ele recebe o caractere “.” , seguido de uma sequência de um ou mais dígitos.
-
-- pv: é representado pelo caractere ‘;’.
-
-Os tokens são representados pelas seguintes expressões regulares:
-
--identificador: [a-z] ( [a-z] | [0-9] | [_])\*
-
-- op_atrib: ‘=’
-- numero: ( [0-9]([0-9])_ ) | ( [0-9]([0-9])_‘.’ [0-9]([0-9])\* )
-- pv: ‘;’
-
-![Autômato](/automaton.png)
+![Autômato](automaton.png)
--- src/main/java/AssignmentAutomaton.java
+++ src/main/java/AssignmentAutomaton.java
@@ -7,20 +7,36 @@ public class AssignmentAutomaton {
     public static void main(String[] args) {
         // Simulador de um automato que reconhece uma atribuição no formato:
         // identificador op_atrib (identificador | numero) pv")
-        // TestCases();
+        // testCases();
 
-        System.out.println(
-                "Escreva uma palavra para ver se atende ao formato: identificador op_atrib (identificador | numero) pv");
-        String word = System.console().readLine();
+        do {
+            System.out.println(
+                    "Escreva uma palavra para ver se atende ao formato: identificador op_atrib (identificador | numero) pv");
+            String word = System.console().readLine();
 
-        Assignment assignment = new Assignment();
-        Boolean valid = assignment.validAssignment(word);
+            Assignment assignment = new Assignment();
+            Boolean valid = assignment.validAssignment(word);
 
-        System.out.println(System.lineSeparator());
-        System.out.println("Palavra: '" + word + "' é " + (valid ? "válida" : "inválida"));
+            System.out.println(System.lineSeparator());
+            System.out.println("Palavra: '" + word + "' é " + (valid ? "válida" : "inválida"));
+        } while (repeatQuest());
     }
 
-    public static void TestCases() {
+    private static Boolean repeatQuest() {
+        System.out.println();
+        System.out.println("Digite 1 e tecle enter para testar outra palavra");
+        System.out.println("Digte qualquer outro caracter para finalizar");
+        String word = System.console().readLine();
+
+        if (word != null)
+            return word.charAt(0) == '1';
+
+        return false;
+    }
+
+    // Caso queira testar comente todas as chamadas acima e descomenta a chamada do
+    // TestCases()
+    public static void testCases() {
         String[] testsCases = { "a=a;", "a=9;", "a=9.9;", "a_=a_;", "a_=aa;", "a_=9a;", "aa=a;", "aa=a_;", "aa=aa;",
                 "aa=a9;", "a9=a;", "a9=a_;", "a9=aa;", "a9=a9;", "a9=aa;", "a=a_;", "a_=aa;", "a_=a9;", "a=99;",
                 "a_=9;", "aa=9;", "a9=9;", "aa=99;", "a_=9;", "a_=99;", "a_=9.9;", "a_=9.99;", "9", "aa", "a==", "a=a",
